da83300b8d528f5f86d029695a075bd160539722
Crowdbotics <engineering@crowdbotics.com>
2023-08-01 06:22:52 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "round-math-dev-106137",
+  url: "https://round-math-dev-106137.botics.co",
+  api: "https://round-math-dev-106137.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
